fb47a31ec5d1ae767aac598e0c55580234c758f1
danil_lans <danimen0001@gmail.com>
2021-06-07 00:16:02 +0300

    transport builder interface

---
+++ pppp3/pppp3/Transports/ITransportsBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace pppp3.Transports
+{
+    public interface ITransportsBuilder
+    {
+        void AddCar(int i);
+        void AddTrain(int i);
+        void AddPlane(int i);
+        void Reset();
+    }
+}
